71112f8e74fa95e4c1a7c882dd2bb76babca7538
angelajanevska <80795984+angelajanevska@users.noreply.github.com>
2023-07-11 02:00:20 +0200

    add standard user

--- MovieTicketsWebApp.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MovieTicketsWebApp.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MovieTicketsWebApp.Data/Identity/StandardUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieTicketsWebApp.Domain.Identity
+{
+    public class StandardUser : IdentityUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
